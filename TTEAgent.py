0f5f2e2572925b3eb9e9b48df1aa5684d62933ec
Anant Asthana <stemanant@gmail.com>
2025-07-27 10:56:42 -0400

    TTE agent instructions

--- TTEAgent.py
+++ TTEAgent.py
@@ -1,2 +1,5 @@
 import pandas as pd 
 print('hello')
+
+# can't use any data if < 20 patients bc might be identified 
+# Finds closest approved drug using AffinityNet and is also in AllOfUs database
